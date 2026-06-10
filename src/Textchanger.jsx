63ad6bebcb93cdaf3d6b607c0ffcd7c46e2c65e9
erplnv3 <erp.lnv3@gmail.com>
2026-06-10 16:32:24 +0530

    test

--- src/Textchanger.jsx
+++ src/Textchanger.jsx
@@ -1251,7 +1251,7 @@ return (
       style={{
         position: "absolute",
         inset: 0,
-        width: "50%",
+        width: "100%",
         height: "100%",
         objectFit: "cover",
         transform: "scaleX(-1)",
