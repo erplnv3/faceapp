e47fffaba58f7937424cc6d509f7b03195536d8f
erplnv3 <erp.lnv3@gmail.com>
2026-06-12 17:20:48 +0530

    ii

--- src/Textchanger.jsx
+++ src/Textchanger.jsx
@@ -263,7 +263,7 @@ const result = await faceapi
     blockedAt &&
     Date.now() - blockedAt < 5 * 60 * 1000
   ) {
-    setMessage(` recently processed`);
+    setMessage(` Recently processed`);
     return;
   }
 
